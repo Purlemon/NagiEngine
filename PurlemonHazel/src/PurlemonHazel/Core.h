28ec7a6d1a9480e9b5bf211a1a911d2e6f17e446
Purlemon <chuimingqingning@gmail.com>
2022-02-13 20:03:37 +0800

    更改为lib

--- PurlemonHazel/src/PurlemonHazel/Core.h
+++ PurlemonHazel/src/PurlemonHazel/Core.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #ifdef PH_PLATFORM_WINDOWS // 在windows生效
-	#ifdef PH_BUILD_DLL // 在PurlemonHazel中定义
-		#define PH_API __declspec(dllexport) // 在PurlemonHazel中导出dll
-	#else 
-		#define PH_API __declspec(dllimport) // 在其他项目中导入dll
-	#endif 
+	#if PH_DYNAMIC_LINK
+		#ifdef PH_BUILD_DLL // 在PurlemonHazel中定义
+			#define PH_API __declspec(dllexport) // 在PurlemonHazel中导出dll
+		#else 
+			#define PH_API __declspec(dllimport) // 在其他项目中导入dll
+		#endif 
+	#else
+		#define PH_API
+	#endif
 #else
 	#error PurlemonHazel only support windows!
 #endif 

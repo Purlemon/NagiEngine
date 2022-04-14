d1c316480ae5e40999d5346bd0e561c258626702
Purlemon <chuimingqingning@gmail.com>
2022-04-14 10:59:03 +0800

    优化batch_vertices的反人类写法，换成了智能指针

--- NagiEngine/src/NagiEngine/Renderer/Renderer2D.cpp
+++ NagiEngine/src/NagiEngine/Renderer/Renderer2D.cpp
@@ -35,13 +35,13 @@ namespace Nagi {
 	// 2D渲染数据
 	struct Renderer2DData
 	{
-		static const ng_uint32 max_quads = 10000;	// 批处理一次DrawCall绘制的最大Quad个数
-		static const ng_uint32 max_vertices = max_quads * 4;
-		static const ng_uint32 max_indices = max_quads * 6;
-		static const ng_uint32 max_texure_slots = 32;	// TODO：查询GPU一次支持多少个纹理单元
+		static const ng_uint32 kMaxQuadsCnt = 100;	// 批处理一次DrawCall绘制的最大Quad个数
+		static const ng_uint32 kMaxVerticesCnt = kMaxQuadsCnt * 4;
+		static const ng_uint32 kMaxIndicesCnt = kMaxQuadsCnt * 6;
+		static const ng_uint32 kMaxTexureSlots = 32;	// TODO：查询GPU一次支持多少个纹理单元
 		
 		// 四边形四个顶点的默认位置，设计成vec4是为了方便矩阵运算, 注意点的w为1
-		const glm::vec4 quad_vertices[4] = {
+		const glm::vec4 kQuadVertices[4] = {
 			{ -0.5f, -0.5f, 0.0f, 1.0f },	// 左下
 			{  0.5f, -0.5f, 0.0f, 1.0f },	// 右下
 			{  0.5f,  0.5f, 0.0f, 1.0f },	// 右上
@@ -49,7 +49,7 @@ namespace Nagi {
 		};
 
 		// 四边形四个顶点的默认纹理坐标
-		const glm::vec2 quad_texcoords[4] = {
+		const glm::vec2 kQuadTexcoords[4] = {
 			{ 0.0f, 0.0f },		// 左下
 			{ 1.0f, 0.0f },		// 右下
 			{ 1.0f, 1.0f },		// 右上
@@ -61,10 +61,10 @@ namespace Nagi {
 		Ref<Shader> texture_shader;
 		Ref<Texture2D> white_texture;
 
-		// 将分配一个QuadVertex数组，QuadVertex连续存储，用quad_vertex_buffer_ptr对每个QuadVertex操作
-		ng_uint32 quad_index_count = 0;
-		QuadVertex* quad_vertex_buffer_base = nullptr;	// QuadVertex数组头指针
-		QuadVertex* quad_vertex_buffer_ptr = nullptr;	// 在QuadVertex数组中移动
+		// 用于批处理，每次DrawCall时把batch_vertices数组数据填充到VertexBuffer里
+		Scope<QuadVertex[]> batch_vertices;	
+		ng_uint32 filled_vertices_cnt = 0;
+		ng_uint32 filled_triangles_cnt = 0;
 
 		std::unordered_map<Ref<Texture2D>, ng_sizei> add_texture_map; // 记录已经添加的纹理
 
@@ -75,9 +75,12 @@ namespace Nagi {
 
 	void Renderer2D::Init()
 	{
-		sData.quad_vertex_array= VertexArray::Create();
+		// VBO -> IBO -> VAO 
+		// ----------------------------------------
 
-		sData.quad_vertex_buffer = VertexBuffer::Create(sData.max_vertices * sizeof(QuadVertex));
+		// 1. 创建根据单次DrawCall顶点数上限创建VertexBuffer，设置Layout
+		sData.quad_vertex_buffer = VertexBuffer::Create(Renderer2DData::kMaxVerticesCnt * sizeof(QuadVertex));
+		
 		sData.quad_vertex_buffer->SetLayout({
 			{ ShaderDataType::Float3, "a_Position" },
 			{ ShaderDataType::Float4, "a_Color" },
@@ -85,13 +88,11 @@ namespace Nagi {
 			{ ShaderDataType::Int, "a_TexIndex" },
 			{ ShaderDataType::Float, "a_TilingFactor" },
 			});
-		sData.quad_vertex_array->AddVertexBuffer(sData.quad_vertex_buffer);
 
-		sData.quad_vertex_buffer_base = new QuadVertex[sData.max_vertices];
-		// 设置顶点索引数组
-		ng_uint32* quad_indices = new ng_uint32[sData.max_indices];
+		// 2. 创建根据单次DrawCall索引数上限自动生成IndexBuffer数组
+		Scope<ng_uint32[]> quad_indices = CreateScope<ng_uint32[]>(Renderer2DData::kMaxIndicesCnt);
 		ng_uint32 offset = 0;
-		for (unsigned int i = 0; i < sData.max_indices; i += 6) {
+		for (unsigned int i = 0; i < Renderer2DData::kMaxIndicesCnt; i += 6) {
 			quad_indices[i + 0] = offset + 0;
 			quad_indices[i + 1] = offset + 1;
 			quad_indices[i + 2] = offset + 2;
@@ -102,31 +103,36 @@ namespace Nagi {
 
 			offset += 4;
 		}
-
-		Ref<IndexBuffer>quad_ib = IndexBuffer::Create(quad_indices, sData.max_indices);
+		Ref<IndexBuffer>quad_ib = IndexBuffer::Create(&quad_indices[0], Renderer2DData::kMaxIndicesCnt);
+		
+		// 3. 创建VertexArray，设置VertexBuffer与IndexBuffer
+		sData.quad_vertex_array = VertexArray::Create();
+		sData.quad_vertex_array->Bind();
+		sData.quad_vertex_array->AddVertexBuffer(sData.quad_vertex_buffer);
 		sData.quad_vertex_array->SetIndexBuffer(quad_ib);
-		delete[] quad_indices;
 
 		// 纹理相关
 		// -------------------------------
 	
-		// 在GPU中用0xffffffff生成一个纹理
+		// 在GPU中用0xffffffff生成一个纹理，用于无纹理的flatcolor
 		sData.white_texture = Texture2D::Create(1, 1);
 		ng_uint32 white_texture_data = 0xffffffff; // {1.0f, 1.0f, 1.0f, 1.0f}
 		sData.white_texture->SetData(&white_texture_data, sizeof(ng_uint32));
 
-		int samplers[sData.max_texure_slots];
-		for (ng_uint32 i = 0; i < sData.max_texure_slots; ++i) {
+		int samplers[Renderer2DData::kMaxTexureSlots];
+		for (ng_uint32 i = 0; i < Renderer2DData::kMaxTexureSlots; ++i) {
 			samplers[i] = i;	// 采样器数组按顺序绑定纹理编号
 		}
 
+		sData.add_texture_map[sData.white_texture] = 0;
+
+		// shader相关
+		// -------------------------------
 		sData.texture_shader = Shader::Create("texture", "assets/shaders/vertex/texture.vert", "assets/shaders/fragment/texture.frag");
 		sData.texture_shader->Bind();
-		sData.texture_shader->SetIntArray("u_Textures", samplers, sData.max_texure_slots);
+		sData.texture_shader->SetIntArray("u_Textures", samplers, sData.kMaxTexureSlots);
 
-		//sData.texture_slots[0] = sData.white_texture;
-		sData.add_texture_map[sData.white_texture] = 0;
-		sData.white_texture->Bind(0);
+		sData.batch_vertices = CreateScope<QuadVertex[]>(Renderer2DData::kMaxVerticesCnt);
 	}
 
 	void Renderer2D::Shutdown()
@@ -139,8 +145,8 @@ namespace Nagi {
 		sData.texture_shader->Bind();
 		sData.texture_shader->SetMat4("u_ProjectionView", camera.GetProjectionViewMatrix());
 
-		sData.quad_index_count = 0;
-		sData.quad_vertex_buffer_ptr = sData.quad_vertex_buffer_base;
+		sData.filled_vertices_cnt = 0;
+		sData.filled_triangles_cnt = 0;
 	}
 
 	void Renderer2D::EndScene()
@@ -150,11 +156,12 @@ namespace Nagi {
 
 	void Renderer2D::Flush()
 	{
-		// 计算所用的顶点数组大小
-		ng_uint32 data_size = sizeof(QuadVertex) * (sData.quad_vertex_buffer_ptr - sData.quad_vertex_buffer_base);
-		sData.quad_vertex_buffer->SetData(sData.quad_vertex_buffer_base, data_size);
+		// 计算所用batch_vertices的大小
+		ng_uint32 data_size = sizeof(QuadVertex) * sData.filled_vertices_cnt;
+		sData.quad_vertex_buffer->SetData(&sData.batch_vertices[0], data_size);
 
-		RenderCommand::DrawIndexed(sData.quad_vertex_array, sData.quad_index_count);
+		RenderCommand::DrawIndexed(sData.quad_vertex_array, sData.filled_triangles_cnt * 3);
+
 		++sData.frame_state.batch_count;
 	}
 
@@ -166,8 +173,8 @@ namespace Nagi {
 
 	void Renderer2D::ResetBatchParams()
 	{
-		sData.quad_index_count = 0;
-		sData.quad_vertex_buffer_ptr = sData.quad_vertex_buffer_base;
+		sData.filled_vertices_cnt = 0;
+		sData.filled_triangles_cnt = 0;
 		sData.add_texture_map.clear();
 		sData.add_texture_map[sData.white_texture] = 0;
 	}
@@ -178,7 +185,7 @@ namespace Nagi {
 	void Renderer2D::DrawQuad(const QuadProps& quad_props, const glm::vec4& color)
 	{
 		// 当前drawcall数据够多了，换下一批
-		if (sData.quad_index_count >= Renderer2DData::max_indices) {
+		if (sData.filled_vertices_cnt >= Renderer2DData::kMaxVerticesCnt) {
 			FlushAndReset();
 		}
 
@@ -192,24 +199,25 @@ namespace Nagi {
 			glm::scale(glm::mat4(1.0f), glm::vec3(quad_props.size.x, quad_props.size.y, 1.0f) / 10.0f) *
 			glm::rotate(glm::mat4(1.0f), glm::radians(quad_props.rotation), glm::vec3(0.0f, 0.0f, 1.0f));
 
+
 		// 对四边形四个顶点分别设置Vertex数据
 		for (ng_sizei i = 0; i < 4; ++i) {
-			sData.quad_vertex_buffer_ptr->position = transform * sData.quad_vertices[i];// 在CPU内完成model矩阵的运算
-			sData.quad_vertex_buffer_ptr->color = color;
-			sData.quad_vertex_buffer_ptr->texcoord = sData.quad_texcoords[i];
-			sData.quad_vertex_buffer_ptr->tex_index = tex_index;
-			sData.quad_vertex_buffer_ptr->tiling_factor = tiling_factor;
-			++sData.quad_vertex_buffer_ptr;
+			QuadVertex& vertex = sData.batch_vertices[sData.filled_vertices_cnt];
+			vertex.position = transform * sData.kQuadVertices[i];// 在CPU内完成model矩阵的运算
+			vertex.color = color;
+			vertex.texcoord = sData.kQuadTexcoords[i];
+			vertex.tex_index = tex_index;
+			vertex.tiling_factor = tiling_factor;
+			++sData.filled_vertices_cnt;
 		}
-		// 四边形 = 两个三角形，顶点索引+6
-		sData.quad_index_count += 6;
+		sData.filled_triangles_cnt += 2;
 
 		++sData.frame_state.quad_count;
 	}
 
 	void Renderer2D::DrawQuad(const QuadProps& quad_props, const Texture2DPorps& tex2d_porps)
 	{
-		if (sData.quad_index_count >= Renderer2DData::max_indices) {
+		if (sData.filled_vertices_cnt >= Renderer2DData::kMaxVerticesCnt) {
 			FlushAndReset();
 		}
 
@@ -223,21 +231,22 @@ namespace Nagi {
 
 		// 如果纹理已经在map中，直接使用；否则加入map分配纹理单元id
 		if (!sData.add_texture_map.count(tex2d_porps.texture)) {
-			ng_sizei texture_unit_id = std::min((ng_sizei)sData.add_texture_map.size(), sData.max_texure_slots);
+			ng_sizei texture_unit_id = std::min((ng_sizei)sData.add_texture_map.size(), Renderer2DData::kMaxTexureSlots);
 			sData.add_texture_map[tex2d_porps.texture] = texture_unit_id;
 			tex2d_porps.texture->Bind(texture_unit_id);
 		}
 		int texture_index = sData.add_texture_map[tex2d_porps.texture];
 
 		for (ng_sizei i = 0; i < 4; ++i) {
-			sData.quad_vertex_buffer_ptr->position = transform * sData.quad_vertices[i];
-			sData.quad_vertex_buffer_ptr->color = color;
-			sData.quad_vertex_buffer_ptr->texcoord = sData.quad_texcoords[i];
-			sData.quad_vertex_buffer_ptr->tex_index = texture_index;
-			sData.quad_vertex_buffer_ptr->tiling_factor = tex2d_porps.tiling_factor;
-			++sData.quad_vertex_buffer_ptr;
+			QuadVertex& vertex = sData.batch_vertices[sData.filled_vertices_cnt];
+			vertex.position = transform * sData.kQuadVertices[i];
+			vertex.color = color;
+			vertex.texcoord = sData.kQuadTexcoords[i];
+			vertex.tex_index = texture_index;
+			vertex.tiling_factor = tex2d_porps.tiling_factor;
+			++sData.filled_vertices_cnt;
 		}
-		sData.quad_index_count += 6;
+		sData.filled_triangles_cnt += 2;
 
 		++sData.frame_state.quad_count;
 	}

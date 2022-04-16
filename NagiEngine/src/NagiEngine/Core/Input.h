41f9926958ced4ae843c000527767d47e30370f1
Purlemon <chuimingqingning@gmail.com>
2022-04-16 17:39:17 +0800

    input不需要运行时绑定

--- NagiEngine/src/NagiEngine/Core/Input.h
+++ NagiEngine/src/NagiEngine/Core/Input.h
@@ -6,25 +6,15 @@
 
 namespace Nagi {
 	// 单例类
-	class NAGI_API Input
+	class Input
 	{
 	public:
-		static bool IsKeyPressed(int keycode) { return instance_->IsKeyPressedImpl(keycode); }
-	
-		static bool IsMouseButtonPressed(int button) { return instance_->IsMouseButtonPressedImpl(button); }
-		static std::pair<float, float> GetMousePosition() { return instance_->GetMousePositionImpl(); }
-		static float GetMouseX() { return instance_->GetMouseXImpl(); }
-		static float GetMouseY() { return instance_->GetMouseYImpl(); }
-	protected:
-		// 子类实现接口
-		virtual bool IsKeyPressedImpl(int keycode) = 0; 
-	
-		virtual bool IsMouseButtonPressedImpl(int button) = 0;
-		virtual std::pair<float, float> GetMousePositionImpl() = 0;
-		virtual float GetMouseXImpl() = 0;
-		virtual float GetMouseYImpl() = 0;
-	private:
-		static Input* instance_;
+		static bool IsKeyPressed(int key);
+
+		static bool IsMouseButtonPressed(int button);
+		static std::pair<float, float> GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
 	};
 
 }
